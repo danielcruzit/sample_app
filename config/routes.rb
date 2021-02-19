fc7fbbccae1c0bedfb908994c49c51687a8899c6
dc <daniel.cruz.code@gmail.com>
2021-02-19 14:54:31 +0000

    created controller whit scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
   #get 'home', to: "static_pages#home" #penso que nao é necessaria por termos a root ja a fazer o mesmo ?!-QA
   get 'help', to: "static_pages#help" 
   get 'about', to: "static_pages#about"
   get 'contacts', to: "static_pages#contact"
   #get 'static_page/login'
-  #get 'static_page/signup'
+  get 'signup', to:"users#new"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,9 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
+
+
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
@@ -10,4 +13,5 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include ApplicationHelper
 end
