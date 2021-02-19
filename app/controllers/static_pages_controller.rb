9112960dc5d3ee8727b039adf2164d13b928d509
dc <daniel.cruz.code@gmail.com>
2021-02-19 14:39:29 +0000

    fixing routes names and link_to's

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -16,4 +16,7 @@ class StaticPagesController < ApplicationController
   def contact
   end
 
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about', to: "static_pages#about"
+  root 'static_pages#home'
+  #get 'home', to: "static_pages#home" #penso que nao é necessaria por termos a root ja a fazer o mesmo ?!-QA
+  get 'help', to: "static_pages#help" 
+  get 'about', to: "static_pages#about"
   get 'contacts', to: "static_pages#contact"
+  #get 'static_page/login'
+  #get 'static_page/signup'
 end
